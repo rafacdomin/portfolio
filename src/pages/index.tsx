feeceb2de052f1709b27907aeadd4e311808d2c3
Rafael Domingues <rafaelcodomingues@gmail.com>
2020-11-02 17:27:57 -0300

    :rocket: Header Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { Container, Header, Menu } from "../styles/pages/index";
+import { FiMenu } from 'react-icons/fi';
+
+import RafaDev from '../assets/Logo.svg';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <Header>
+        <a href="/">
+          <img src={RafaDev}/>
+        </a>
+
+        <Menu>
+          <a href="#about">Sobre</a>
+          <a href="#projects">Projetos</a>
+          <a href="#blog">Blog</a>
+          <a href="#contact">Contato</a>
+          <FiMenu size={24}/>
+        </Menu>
+      </Header>
+    </Container>
   )
 }
--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -1,5 +1,49 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  max-width: 1120px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 4px;
+
+  img {
+    height: 34px;
+    width: 126px;
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 300;
+    font-size: 16px;
+    color: #000;
+    text-decoration: none;
+    transition: color 0.2s;
+    
+    & + a{
+      margin-left: 24px;
+    }
+
+    &:hover{
+      color: #666;
+    }
+  }
+
+  svg {
+    margin-left: 24px;
+
+    &:hover{
+      cursor: pointer;
+    }
+  }
 `;
