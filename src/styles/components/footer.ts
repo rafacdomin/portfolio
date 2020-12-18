e920c151043dfef6aedd4fe7717ecff8f417a89e
Rafael Domingues <rafaelcodomingues@gmail.com>
2020-12-17 21:07:32 -0300

    :art: Footer css responsive

--- src/styles/components/footer.ts
+++ src/styles/components/footer.ts
@@ -1,19 +1,29 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-  margin-top: 120px;
-  padding-bottom: 8px;
-  margin-bottom: 1px;
-  border-bottom: 4px solid #FF7B32;
+  padding: 0 1.6rem;
+  margin-top: 12rem;
+  padding-bottom: 0.8rem;
+  margin-bottom: 0.1rem;
+  border-bottom: 0.4rem solid #FF7B32;
   max-width: 1120px;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
 
+  img {
+    height: 80%;
+  }
+
   span {
     font-weight: 400;
-    font-size: 12px;
+    font-size: 1.2rem;
     color: #666;
+  }
+
+  @media(min-width: 1100px) {
+    padding-right: 0;
+    padding-left: 0;
   }
 `;
