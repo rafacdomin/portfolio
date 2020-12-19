6e9c72befdbf357dfd552070cc0753ce7c85b120
Rafael Domingues <rafaelcodomingues@gmail.com>
2020-12-19 15:05:08 -0300

    :art: Projects responsivity

--- src/styles/components/projects.ts
+++ src/styles/components/projects.ts
@@ -2,21 +2,22 @@ import styled from 'styled-components';
 
 export const Projects = styled.section`
   max-width: 1120px;
-  width: 100%;
+  width: 100vw;
 
-  margin-top: 72px;
+  margin-top: 5.6rem;
 
   h2 {
+    padding: 0 1.6rem;
     max-width: fit-content;
     margin-left: auto;
 
-    font-size: 36px;
+    font-size: 2.4rem;
     font-weight: 400;
   }
 `;
 
 export const Carousel = styled.div`
-  margin-top: 80px;
+  margin-top: 4rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -24,36 +25,56 @@ export const Carousel = styled.div`
   a {
     text-decoration: none;
 
-    transition: all 0.2s;
+    transition: all 0.5s;
+    transform: translateY(0rem);
 
     &:hover {
       opacity: 0.9;
-      transform: translateY(-4px);
+      transform: translateY(-0.4rem);
 
       span {
         display: unset;
       }
     }
 
+    img {
+      width: 100%;
+    }
+
     span {
-      display: none;
+      display: unset;
       position: absolute;
-      bottom: 4px;
+      bottom: 0.4rem;
       left: 0;
 
-      background: rgba(0, 0, 0, 0.6);
-      padding: 16px;
+      background: rgba(0, 0, 0, 0.4);
+      padding: 1.2rem 1.6rem;
       width: 100%;
-      border-radius: 4px;
+      border-radius: 0.4rem;
 
       color: #fff;
-      font-size: 21px;
+      font-size: 1.6rem;
       font-weight: bold;
 
       p {
-        margin: 8px 0;
-        font-size: 14px;
+        display: none;
+        margin: 0.4rem 0;
+        font-size: 1.2rem;
         font-weight: normal;
+
+        @media(min-width: 450px) {
+          display: block;
+        }
+      }
+
+      @media(min-width: 1100px) {
+        display: none;
+        padding: 1.6rem;
+        font-size: 2.4rem;
+
+        p {
+          font-size: 1.6rem;
+        }
       }
     }
   }
@@ -64,7 +85,6 @@ export const Carousel = styled.div`
     justify-content: center;
 
     border-radius: 50%;
-    padding: 4px;
     border: none;
     background: none;
 
